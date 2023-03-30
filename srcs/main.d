387ff31abe64949af7fe216c774193dffdd96c0a
halvarez <hansalvarezherencia@protonmail.com>
2023-03-30 16:24:28 +0200

    bkp

---
+++ srcs/main.d
@@ -0,0 +1,3 @@
+srcs/main.o: srcs/main.cpp srcs/server/Server.hpp
+
+srcs/server/Server.hpp:
---
+++ srcs/server/Server.d
@@ -0,0 +1,3 @@
+srcs/server/Server.o: srcs/server/Server.cpp srcs/server/Server.hpp
+
+srcs/server/Server.hpp:
