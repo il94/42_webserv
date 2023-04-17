8d385da7c1353964d9112390dfe063bc4cbbd090
halvarez <hansalvarezherencia@protonmail.com>
2023-04-17 13:48:10 +0200

    back-up before config file into server

--- obj/cgi/CGI.d
+++
@@ -1,6 +0,0 @@
-obj/cgi/CGI.o: srcs/cgi/CGI.cpp srcs/cgi/CGI.hpp \
-  srcs/cgi/../http/request/Request.hpp
-
-srcs/cgi/CGI.hpp:
-
-srcs/cgi/../http/request/Request.hpp:
--- obj/http/request/Request.d
+++
@@ -1,4 +0,0 @@
-obj/http/request/Request.o: srcs/http/request/Request.cpp \
-  srcs/http/request/Request.hpp
-
-srcs/http/request/Request.hpp:
