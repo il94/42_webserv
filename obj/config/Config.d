4902991ceab1f93b235d40818f1cfdbf588ddc12
halvarez <hansalvarezherencia@protonmail.com>
2023-05-16 18:49:04 +0200

    +gitignore

--- obj/config/Config.d
+++
@@ -1,8 +0,0 @@
-obj/config/Config.o: srcs/config/Config.cpp srcs/config/Config.hpp \
-  srcs/config/Location.hpp srcs/config/../utils/utils.hpp
-
-srcs/config/Config.hpp:
-
-srcs/config/Location.hpp:
-
-srcs/config/../utils/utils.hpp:
--- obj/config/Location.d
+++
@@ -1,6 +0,0 @@
-obj/config/Location.o: srcs/config/Location.cpp srcs/config/Location.hpp \
-  srcs/config/../utils/utils.hpp
-
-srcs/config/Location.hpp:
-
-srcs/config/../utils/utils.hpp:
